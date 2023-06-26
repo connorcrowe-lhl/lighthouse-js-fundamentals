1294a82de6eacecfe3382d7b1b35b0676ab395ce
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-06-26 12:09:07 -0400

    Add temperature range

--- conditionals.js
+++ conditionals.js
@@ -8,7 +8,9 @@ if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
-} else {
+} else if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+else {
   console.log("Short sleeves are fine.");
 }
 
